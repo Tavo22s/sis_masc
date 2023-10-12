67c9e07cff3dba5563ad3879bb4172b63deddf67
Tavo22s <gusedpp@gmail.com>
2023-10-11 21:59:45 -0500

    clientv1.1

--- app/Http/Livewire/ClienteComponent.php
+++ app/Http/Livewire/ClienteComponent.php
@@ -3,11 +3,13 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Cliente;
 
 class ClienteComponent extends Component
 {
     public function render()
     {
+        $clientes = Cliente::all();
         return view('livewire.cliente-component');
     }
 }
