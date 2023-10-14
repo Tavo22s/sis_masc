dccd39915c97aa6f3327e16583939fdc4bde3d8f
Tavo22s <115854166+Tavo22s@users.noreply.github.com>
2023-10-13 19:03:37 -0500

    mejorvista

--- resources/views/livewire/cliente-component.blade.php
+++ resources/views/livewire/cliente-component.blade.php
@@ -1,68 +1,82 @@
 <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg ">
-    <div class="container-fluid py-4">
-      <div class="row">
-        <div class="col-12">
-          <div class="card mb-4">
-            <div class="card-header">
-              <h6>LISTA DE CLIENTES</h6>
-              <div class="row">
-                <div class="col-sm-5">
-                  <label class="form-label text-sm" for="buscar">Buscar</label>
-                  <div class="input-group">
-                    <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
-                    <input id="busqueda" wire:model.live="busqueda" type="text" class="form-control" placeholder="Buscar...">
+  <div class="row">
+      <div class="col-12">
+          <div class="card mb-4 mx-4">
+              <div class="card-header pb-0">
+                  <div class="d-flex flex-row justify-content-between">
+                      <div>
+                          <h5 class="mb-0">Lista de Clientes</h5>
+                      </div>
                   </div>
-                </div>
-                <div class="col-sm-5">
-                  <label class="form-label" for="showToastPlacement">&nbsp;</label>
-                  <button class="btn btn-primary d-block active mb-0 text-white" type="button">Agregar Cliente</button>
-                </div>
+                  <div class="d-flex flex-row justify-content-between py-2">
+                    <div class="col-sm-7">
+                      <div class="input-group">
+                        <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
+                        <input id="busqueda" wire:model.live="busqueda" type="text" class="form-control" placeholder="Buscar...">
+                      </div>
+                    </div>
+                    <a href="#" class="btn bg-gradient-primary btn-sm mb-0" type="button">+&nbsp; Agregar Cliente</a>
+                  </div>
               </div>
-            </div>
-            <div class="card-body px-0 pt-0 pb-2 gy-3">
-              <div class="table-responsive p-0">
-                <table class="table align-items-center mb-0">
-                  <thead>
-                    <tr>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Nombre</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Correo</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">DNI</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Telefono 1</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Telefono 2</th>
-                      <th class="text-secondary opacity-7"></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach($clientes as $c)
-                      <tr>
-                        <td>
-                          <p class="text-xs font-weight-bold mb-0">{{ $c->nombre_completo }}</p>
-                        </td>
-                        <td>
-                          <p class="text-xs font-weight-bold mb-0">{{ $c->correo }}</p>
-                        </td>
-                        <td>
-                          <p class="text-xs font-weight-bold mb-0">{{ $c->dni }}</p>
-                        </td>
-                        <td>
-                          <p class="text-xs font-weight-bold mb-0">{{ $c->telefono_1 }}</p>
-                        </td>
-                        <td>
-                          <p class="text-xs font-weight-bold mb-0">{{ $c->telefono_2 }}</p>
-                        </td>
-                        <td class="align-middle">
-                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                            Edit
-                          </a>
-                        </td>
-                      </tr>
-                    @endforeach
-                  </tbody>
-                </table>
+              <div class="card-body px-0 pt-0 pb-2">
+                  <div class="table-responsive p-0">
+                      <table class="table align-items-center mb-0">
+                          <thead>
+                              <tr>
+                                  <th class="text-center text-secondary text-xs font-weight-bolder opacity-7">
+                                      Nombre
+                                  </th>
+                                  <th class="text-center text-secondary text-xs font-weight-bolder opacity-7 ps-2">
+                                      Correo
+                                  </th>
+                                  <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                      DNI
+                                  </th>
+                                  <th class="text-center text-secondary text-secondary text-xs font-weight-bolder opacity-7">
+                                      Telefono 1
+                                  </th>
+                                  <th class="text-center text-secondary text-secondary text-xs font-weight-bolder opacity-7">
+                                      Telefono 2
+                                  </th>
+                                  <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
+                                      Action
+                                  </th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              @foreach ($clientes as $cliente)
+                                <tr>
+                                  <td class="text-center">
+                                      <p class="text-xs font-weight-bold mb-0">{{ $cliente->nombre_completo }}</p>
+                                  </td>
+                                  <td class="text-center">
+                                    <p class="text-xs font-weight-bold mb-0">{{ $cliente->correo }}</p>
+                                </td>
+                                  <td class="text-center">
+                                      <p class="text-xs font-weight-bold mb-0">{{ $cliente->dni }}</p>
+                                  </td>
+                                  <td class="text-center">
+                                      <p class="text-xs font-weight-bold mb-0">{{ $cliente->telefono_1 }}</p>
+                                  </td>
+                                  <td class="text-center">
+                                      <p class="text-xs font-weight-bold mb-0">{{ $cliente->telefono_2 }}</p>
+                                  </td>
+                                  <td class="text-center">
+                                      <a href="#" class="mx-3" data-bs-toggle="tooltip" data-bs-original-title="Editar Cliente">
+                                          <i class="fas fa-user-edit text-secondary"></i>
+                                      </a>
+                                      <span>
+                                          <i class="cursor-pointer fas fa-trash text-secondary"></i>
+                                      </span>
+                                  </td>
+                                </tr>
+                              @endforeach
+                          </tbody>
+                      </table>
+                  </div>
               </div>
-            </div>
           </div>
-        </div>
       </div>
-    </div>
-  </main>
+  </div>
+</main>
+
